14a33e273899ec6f921b71d0188808e0dd0507b3
Your Name <you@example.com>
2021-01-13 22:05:54 +0800

    upload create authorities

--- server/service/sys_authority.go
+++ server/service/sys_authority.go
@@ -12,8 +12,8 @@ import (
 // @return: err error, authority model.SysAuthority
 func CreateAuthority(auth model.SysAuthority)(err error, authority model.SysAuthority){
 	var authorityBox model.SysAuthority
-	if err :=global.GVA_DB.QueryRow("select * from sys_authority where authority_id = ? limit 1;",auth.AuthorityId).Scan(&authorityBox.CreatedAt, &authorityBox.UpdatedAt, &authorityBox.DeletedAt, &authorityBox.AuthorityId, &authorityBox.AuthorityName, &authorityBox.ParentId); err !=nil{
-		_, err := global.GVA_DB.Exec("insert into sys_authority ('CreatedAt','UpdatedAt','DeletedAt','AuthorityId','AuthorityName','ParentId') values(?,?,?,?,?,?)",auth.CreatedAt,auth.UpdatedAt,auth.DeletedAt,auth.AuthorityId,auth.AuthorityName,auth.ParentId)
+	if err :=global.GVA_DB.QueryRow("select * from sys_authorities where authority_id = $1 limit 1;",auth.AuthorityId).Scan(&authorityBox.CreatedAt, &authorityBox.UpdatedAt, &authorityBox.DeletedAt, &authorityBox.AuthorityId, &authorityBox.AuthorityName, &authorityBox.ParentId); err !=nil{
+		_, err := global.GVA_DB.Exec("insert into sys_authorities (Created_at,Updated_At,Deleted_At,Authority_Id,Authority_Name,Parent_Id) values($1,$2,$3,$4,$5,$6)",auth.CreatedAt,auth.UpdatedAt,auth.DeletedAt,auth.AuthorityId,auth.AuthorityName,auth.ParentId)
 		return err, auth
 	} else {
 		return errors.New("存在相同api"), auth
