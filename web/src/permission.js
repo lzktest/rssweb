ed596bb1d998a43751190d8594a0102250e0bb2a
unknown <aaa@example.com>
2021-05-25 18:35:04 +0800

    upload router

--- web/src/permission.js
+++ web/src/permission.js
@@ -4,43 +4,50 @@ import getPageTitle from '@/utils/page'
 
 let asyncRouterFlag = 0
 
-const whiteList  = ['login','init','public','rssview']
+// token验证白名单
+const whiteList  = ['login','init','public']
+// 公共目录（登录不影响）
+const publicList = ['rssview']
 router.beforeEach(async(to, from, next) => {
     const token = store.getters['user/token']
     // 在白名单中的判断情况
     // 修改网页标签名称
     document.title = getPageTitle(to.meta.title)
-    if (whiteList.indexOf(to.name) > -1){
-        if (token) {
-            next({ name: store.getters['user/userInfo'].authority.defaultRouter })
-        } else {
-            next()
-        }
+    if (publicList.indexOf(to.name) > -1){
+        next()
     } else {
-        // 不在白名单中并且已经登陆的时候
-        if (token) {
-            // 添加flag防止多次获取动态路由和栈溢出
-            if (!asyncRouterFlag && store.getters['router/asyncRouters'].length == 0){
-                asyncRouterFlag++
-                await store.dispatch('router/SetAsyncRouter')
-                const asyncRouters = store.getters['router/asyncRouters']
-                //router.addRoute(asyncRouters)
-                for(var i=0;i<asyncRouters.length;i++){
-                    router.addRoute(asyncRouters[i])
-                }
-                next({...to,replace: true})
+        if (whiteList.indexOf(to.name) > -1){
+            if (token) {
+                next({ name: store.getters['user/userInfo'].authority.defaultRouter })
             } else {
                 next()
             }
-        }
-        // 不在白名单中且未登陆的时候
-        if (!token) {
-            next({
-                name: "login",
-                query: {
-                    redirect: document.location.hash
+        } else {
+            // 不在白名单中并且已经登陆的时候
+            if (token) {
+                // 添加flag防止多次获取动态路由和栈溢出
+                if (!asyncRouterFlag && store.getters['router/asyncRouters'].length == 0){
+                    asyncRouterFlag++
+                    await store.dispatch('router/SetAsyncRouter')
+                    const asyncRouters = store.getters['router/asyncRouters']
+                    //router.addRoute(asyncRouters)
+                    for(var i=0;i<asyncRouters.length;i++){
+                        router.addRoute(asyncRouters[i])
+                    }
+                    next({...to,replace: true})
+                } else {
+                    next()
                 }
-            })
+            }
+            // 不在白名单中且未登陆的时候
+            if (!token) {
+                next({
+                    name: "login",
+                    query: {
+                        redirect: document.location.hash
+                    }
+                })
+            }
         }
     }
 } 
